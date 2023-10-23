51e01c37c3f46db4b3135fd3dff58d8c860bfbeb
Tom Wu <twu@lcs.on.ca>
2023-10-23 09:04:07 -0400

    Initial Commit

---
+++ Magic8BallOnPhone/Magic8BallOnPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallOnPhoneApp.swift
+//  Magic8BallOnPhone
+//
+//  Created by Tom Wu on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallOnPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
